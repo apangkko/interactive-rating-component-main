b7672c917f7299271a6c9d6332e4afcf1506e2e0
APANG <apangkko@gmail.com>
2022-09-15 19:26:36 +0700

    Submit solution

---
+++ script.js
@@ -0,0 +1,27 @@
+const items = document.querySelectorAll('.listItem');
+const rate = document.querySelector('.ratingCard');
+const thanks = document.querySelector('.thanksCard');
+const ratingPoint = document.querySelector('.ratingPoint');
+let point;
+
+function getRatingPoint(e) {
+    point = e.innerHTML;
+
+    for (let i = 0; i < items.length; i++) {
+        console.log(items[i].innerHTML);
+        if (items[i].innerHTML == point) {
+            items[i].classList.add('active');
+        } else {
+            items[i].classList.remove('active');
+        }
+    }
+}
+
+function submitRating() {
+    if(point != null) {
+        rate.style.display = 'none';
+        thanks.style.display = 'block';
+        ratingPoint.innerHTML = point;
+    }
+    
+}
